f793f2b4c2cf3c9d304672387417fd7104811253
Joseph Simpson <jjs0sbw@gmail.com>
2016-08-22 15:15:17 -0700

    add id to model

--- js/n1/models/structure.js
+++ js/n1/models/structure.js
@@ -3,7 +3,12 @@
   'use strict'
   // Stucture Model
   // ----------
+  var globalCounterId = 1;
   N1.Models.Structure = Backbone.Model.extend({
+    initialize: function () {
+        this.set('id', globalCounterId);
+        globalCounterId += 1;
+    },
     defaults: function () {
       return {
         size: '0',
